4b371b8126fe81af46ee44dcf5b1b403a2387644
okokcoder <rrsatogi@gmail.com>
2025-05-13 15:41:07 +0000

    save

---
+++ 05/4.js
@@ -0,0 +1,12 @@
+// const myObject = {
+//     js : "javascript",
+//     cpp : "c++",
+//     rb : "ruby"
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} used for ${myObject[key]}`);
+    
+// }
+
+const lang = ["java","c++","js"]
